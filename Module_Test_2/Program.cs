45d721e8246def71e502f9c4330c770565294841
AntonKobziev <kobziev.aa@gmail.com>
2019-05-28 20:43:41 +0300

    some change

---
+++ Module_Test_2/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_Test_2
+{
+    class File
+    {
+        public string Name;
+        public string Size;
+        public string Extention;
+
+        protected virtual void Parse(string text)
+        {
+            //
+        }
+
+
+    }
+    class TextFiles : File
+    {
+        public string Content;
+
+        public TextFiles(string text)
+        {
+            Parse(text);
+        }
+
+        protected override void Parse(string text)
+        {
+            base.Parse(text);
+
+            // 
+        }
+    }
+    class Movies : File
+    {
+        
+        public string Resolution;
+        public string Length;
+        public Movies(string text)
+        {
+            Parse(text);
+        }
+
+        protected override void Parse(string text)
+        {
+            base.Parse(text);
+
+            // 
+        }
+    }
+    class Images : File
+    {     
+        public string Resolution;
+        public Images(string text)
+        {
+            Parse(text);
+        }
+
+        protected override void Parse(string text)
+        {
+            base.Parse(text);
+
+            // 
+        }
+
+    }
+    class StringParse
+    {
+        public string[] ArrString { get; set; }
+
+
+        public File[] ArrInput(string input)
+        {
+            string[] arrString = input.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+            File[] arrFiles = new File[arrString.Length];
+            int found = 0;
+            for (int i = 0; i < arrString.Length; i++)
+            {
+                if (arrString[i].StartsWith("Text:"))
+                {
+                    found = arrString[i].IndexOf(":");
+                    arrFiles[i] = new TextFiles(arrString[i].Substring(found + 1));
+                }
+                else if (arrString[i].StartsWith("Image:"))
+                {
+                    found = arrString[i].IndexOf(":");
+                    arrFiles[i] = new Images(arrString[i].Substring(found + 1));
+                }
+                else if (arrString[i].StartsWith("Movie:"))
+                {
+                    found = arrString[i].IndexOf(":");
+                    arrFiles[i] = new Movies(arrString[i].Substring(found + 1));
+                }
+
+            }
+           
+            return arrFiles;
+        }
+
+
+
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String text = @"Text: file.txt(6B); Some string content
+                            Image: img.bmp(19MB); 1920х1080
+                            Text:data.txt(12B); Another string
+                            Text:data1.txt(7B); Yet another string
+                             Movie:logan.2017.mkv(19GB); 1920х1080; 2h12m";
+            StringParse stringInput = new StringParse();
+
+
+
+        }
+    }
+}
